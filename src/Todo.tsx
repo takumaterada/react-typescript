49e0df5f89c1ce3dbe3075c6089230dcd9c84a3b
takuma terada <nsf3021@gmail.com>
2022-11-21 16:21:50 +0900

    実践編終了

---
+++ src/Todo.tsx
@@ -0,0 +1,8 @@
+import { TodoType } from "./types/todo";
+import { FC } from "react";
+
+export const Todo: FC<Omit<TodoType, "id">> = (props) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
+};
